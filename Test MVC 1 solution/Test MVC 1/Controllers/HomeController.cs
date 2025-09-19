0f4323114b064ea4722a06df43c5b42f435450c2
mohamed sabry <mohamedsabryoda@gmail.com>
2025-09-19 03:58:02 +0300

    Task 1 MVC

---
+++ Test MVC 1 solution/Test MVC 1/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Test_MVC_1.Models;
+
+namespace Test_MVC_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+        public string show() {
+            return $"Hello";
+                }
+    }
+}
